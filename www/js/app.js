8fbb062a52a3452800ed3dfdee37d9852ddf5abb
Harry Wynn-Williams <harrywynnwilliams@googlemail.com>
2016-06-17 13:51:35 +0100

    user login and sign up added

--- www/js/app.js
+++ www/js/app.js
@@ -1,6 +1,5 @@
+angular.module('werewolvesTwo', ['ionic', 'ng-token-auth'])
 
-  $apiUrl = 'http://localhost:3000';
-  angular.module('starter', ['ionic', 'ng-token-auth'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -13,110 +12,27 @@
     }
   });
 })
-.config(function($authProvider) {
-$authProvider.configure({
-      apiUrl:                  $apiUrl,
-      tokenValidationPath:     '/auth/validate_token',
-      signOutUrl:              '/auth/sign_out',
-      emailRegistrationPath:   '/auth',
-      accountUpdatePath:       '/auth',
-      accountDeletePath:       '/auth',
-      confirmationSuccessUrl:  window.location.href,
-      passwordResetPath:       '/auth/password',
-      passwordUpdatePath:      '/auth/password',
-      passwordResetSuccessUrl: window.location.href,
-      emailSignInPath:         '/auth/sign_in',
-      storage:                 'localStorage',
-      forceValidateToken:      false,
-      validateOnPageLoad:      true,
-      proxyIf:                 function() { return false; },
-      proxyUrl:                '/proxy',
-      omniauthWindowType:      'sameWindow',
-      authProviderPaths: {
-        github:   '/auth/github',
-        facebook: '/auth/facebook',
-        google:   '/auth/google'
-      },
-      tokenFormat: {
-        "access-token": "{{ token }}",
-        "token-type":   "Bearer",
-        "client":       "{{ clientId }}",
-        "expiry":       "{{ expiry }}",
-        "uid":          "{{ uid }}"
-      },
-      cookieOps: {
-        path: "/",
-        expires: 9999,
-        expirationUnit: 'days',
-        secure: false,
-        domain: 'domain.com'
-      },
-      createPopup: function(url) {
-        return window.open(url, '_blank', 'closebuttoncaption=Cancel');
-      },
-      parseExpiry: function(headers) {
 
-        return (parseInt(headers.expiry) * 1000) || null;
-      },
-      handleLoginResponse: function(response) {
-        return response.data;
-      },
-      handleAccountUpdateResponse: function(response) {
-        return response.data;
-      },
-      handleTokenValidationResponse: function(response) {
-        return response.data;
-      }
-    });
-  })
+
 .config(function($authProvider, $stateProvider, $urlRouterProvider) {
   $stateProvider
-
-  .state('app', {
-     url: '/app',
-     abstract: true,
-     templateUrl: 'templates/menu.html',
-     controller: 'AppCtrl'
+    .state('sign_up', {
+      url: '/sign_up',
+      templateUrl: 'views/user/sign_up.html',
+      controller: 'UserController'
     })
-    .state('signup', {
-      url: '/sign-up',
-      templateUrl: 'templates/user/sign-up.html',
-      controller: 'AuthController as ctrl'
-
+    .state('sign_in', {
+      url: '/sign_in',
+      templateUrl: 'views/user/sign_in.html'
     })
-    .state('login', {
-      url: '/login',
-      templateUrl: 'templates/user/login.html',
-       controller: 'AuthController as ctrl'
-    })
-    .state('app.signUp', {
-  url: '/sign-up',
-  views: {
-    'menuContent': {
-      templateUrl: 'templates/user/sign-up.html',
-      controller: 'AuthController as ctrl'
-    }
-  }
-})
-
-.state('app.login', {
-  url: '/login',
-  views: {
-    'menuContent': {
-      templateUrl: 'templates/user/login.html',
-      controller: 'AuthController as ctrl'
-    }
-  }
-})
     .state('werewolves', {
-      url: '/werewolves',
-      templateUrl: 'templates/werewolves.html'
+      url: '/',
+      templateUrl: 'views/werewolves.html'
     });
 
+    $urlRouterProvider.otherwise('/');
 
-  $urlRouterProvider.otherwise('/');
-
-  $authProvider.configure({
+    $authProvider.configure({
       apiUrl: 'http://localhost:3000'
   });
 
---
+++ www/js/controllers/UserController.js
@@ -0,0 +1,13 @@
+angular.module('werewolvesTwo')
+.controller('UserController', function($scope, $auth, $state){
+  $scope.handleRegBtnClick = function() {
+    $auth.submitRegistration($scope.registrationForm)
+      .then(function(resp) {
+       $state.go('search');
+      })
+      .catch(function(resp) {
+      console.log(resp);
+      });
+  };
+
+});
