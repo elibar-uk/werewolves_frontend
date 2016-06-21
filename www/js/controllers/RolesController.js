e12d77d4f5d56a47cd688077500fd089b5c8ba75
Hanna <hannaeberhoefer@yahoo.com>
2016-06-21 15:50:35 +0100

    Changed structure

--- www/js/controllers/RolesController.js
+++ www/js/controllers/RolesController.js
@@ -1,9 +1,8 @@
 angular.module('werewolvesTwo', ['ionic'])
-
-.controller('RolesController', function($scope) {
+  .controller('RolesController', function($scope) {
   $scope.cards = [
     { image: 'assets/img/villager.png',
-    text: 'The most commonplace role, a simple Villager, spends the game trying to root out who they believe the werewolves (and other villagers) are. While they do not need to lie, the role requires players to keenly sense and point out the flaws or mistakes of their fellow players. Someone is speaking too much? Could mean they are a werewolf. Someone it is not speaking enough? Could mean the same thing. It all depends on the people you are playing with, and how well you know them' },
+  text: '' },
     { image: 'assets/img/werewolf.png',
   text: '' },
     { image: 'assets/img/little-girl.png',
