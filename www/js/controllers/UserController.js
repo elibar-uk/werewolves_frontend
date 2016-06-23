ff46cd4e9dcf36d3a94d9141340004720e16555c
Hanna <hannaeberhoefer@yahoo.com>
2016-06-23 10:26:56 +0100

    Tried to fix error

--- www/js/controllers/UserController.js
+++ www/js/controllers/UserController.js
@@ -1,21 +1,23 @@
 angular.module('werewolvesTwo')
-.controller('UserController', function($location, $rootScope, $scope, $auth, $state, $window){
-
+.controller('UserController', function($location, $rootScope, $scope, $auth, $state){
   $scope.handleRegBtnClick = function() {
     $auth.submitRegistration($scope.registrationForm)
       .then(function(resp) {
-      $location.path('/app');
-      $window.location.reload();
+      $location.path('/app')
       //  $state.go('app');
       })
       .catch(function(resp) {
-        //error pages
+
+        console.log("workign", resp.statusText)//error pages
       });
     };
+
+
+
     $scope.handleLoginBtnClick = function() {
       $auth.submitLogin($scope.loginForm)
         .then(function(resp) {
-           $state.go('/app');
+           $state.go('/');
         })
         .catch(function(resp) {
           // handle error response
@@ -40,33 +42,11 @@ angular.module('werewolvesTwo')
         });
     };
 
-    // Upload Picture on file select or drop
-    // $scope.upload = function (file) {
-    //   Upload.upload({
-    //     url: 'avatars/' + avatar.id + '.json',
-    //     method: 'PUT',
-    //     headers: { 'Content-Type': false },
-    //     fields: {
-    //       'avatar[title]': avatar.title,
-    //       'avatar[body]': avatar.body,
-    //       'avater[image]': file
-    //     },
-    //     file: file,
-    //     sendFieldsAs: 'json'
-    //   }).then(function (resp) {
-    //     console.log('Success ' + resp.config.file.name + 'uploaded. Response: ' + resp.data);
-    //   }, function (resp) {
-    //     console.log('Error status: ' + resp.status);
-    //   }, function (evt) {
-    //     var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
-    //     console.log('progress: ' + progressPercentage + '% ' + evt.config.file.name);
-    //   });
-    // };
-
 
     $rootScope.$on('auth:login-success', function(ev, user) {
       $scope.user = user;
-      $location.path('/');
+      alert('Welcome ', user.email);
+      $location.path('/')
     });
 
     $scope.$on('devise:new-registration', function (e, user){
@@ -86,6 +66,7 @@ angular.module('werewolvesTwo')
     //   alert('auth failed because', reason.errors[0]);
     // });
     $rootScope.$on('auth:logout-success', function(ev) {
+      alert('goodbye');
       $location.path('/sign_out');
     });
   });
